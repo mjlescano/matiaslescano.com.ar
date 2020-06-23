de81d1ad8c160051f31d5104dda0665292c0de41
Matías Lescano <mjlescano@protonmail.com>
2020-06-23 00:38:08 -0300

    Fix module load

--- els/main.js
+++ els/main.js
@@ -4,5 +4,5 @@
   container.id = id
   document.body.appendChild(container)
   const particles = await window.tsParticles
-    .loadJSON(id, 'particles.json')
+    .loadJSON(id, '/els/particles.json')
 })()
